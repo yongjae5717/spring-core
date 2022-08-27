a89e7a606838d312c97c6289ba4eb42c00e1977d
yongjae5717 <yjk@YJui-MacBookPro.local>
2022-08-27 14:49:32 +0900

    print bean & application bean test

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -5,10 +5,12 @@ import hello.core.member.Member;
 
 public class RateDiscountPolicy implements DiscountPolicy{
 
+    //할인률 정의
     private int discountPercent = 10;
 
     @Override
     public int discount(Member member, int price) {
+        // 10% 할인을 적용하는 알고리즘
         if (member.getGrade() == Grade.VIP){
             return price * discountPercent / 100 ;
         }
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -3,7 +3,6 @@ package hello.core.member;
 
 import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
